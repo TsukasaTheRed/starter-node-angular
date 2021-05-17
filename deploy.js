732c1bb160538c370e571ba8f8f6ebba2e22a61e
Darren <Tsukasa589@gmail.com>
2021-05-17 03:01:33 -0400

    IP Update
    Updated IP in deploy.js to match new instance IP

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.172.42.100',
+      host: '3.89.247.34',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
